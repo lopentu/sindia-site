560d71a12bd56deb010f2188062438f376b3ac24
teewhy <tyleryeh047@gmail.com>
2025-11-26 21:47:07 +0800

    fix: base path

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
-  basePath: "/sindia-site",
+  basePath: isProd ? "/<your-repo-name>" : "",
+  assetPrefix: isProd ? "/<your-repo-name>/" : "",
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
+
