85bdb6f30e898a5eea9b4230a4b17aa53ff1a345
teewhy <tyleryeh047@gmail.com>
2025-11-26 21:57:37 +0800

    fix: next.config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
+// next.config.ts or next.config.mjs/next.config.js
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
-  basePath: isProd ? "/<your-repo-name>" : "",
-  assetPrefix: isProd ? "/<your-repo-name>/" : "",
+  basePath: isProd ? "/sindia-site" : "",
+  assetPrefix: isProd ? "/sindia-site/" : "",
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
-
